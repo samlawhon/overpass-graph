aa2e5cd9ddeae21988914a218edbf282b8905c86
samlawhon <samuel.lawhon97@gmail.com>
2020-10-05 16:33:20 -0400

    Fixed primary function documentation

--- lib/overpass_graph.rb
+++ lib/overpass_graph.rb
@@ -3,7 +3,12 @@ require_relative './overpass_graph/create_vertex_set'
 require_relative './overpass_graph/create_adjacency_hash'
 
 def overpass_graph(north, east, south, west, directed: true, filter_by_allowing: true, filtered_values:[], metric: false)
-
+    '''
+    Primary function for the library. The user may specify whether to filter by allowing or not, in addition to  a few other options.
+    If they choose to filter by allowing certain road types, the get roads function will be passed an empty array for disallowed values.
+    If they choose to filter by disallowing certain road types, the get roads function will be passed an empty array for the allowed values.
+    :return: adjacency map representation of a graph
+    '''
     allowed_values = []
     disallowed_values = []
 
