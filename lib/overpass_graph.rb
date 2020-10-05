311c939462c4d03406e85519ba069220b8015be0
Sam Lawhon <67020783+samlawhon@users.noreply.github.com>
2020-10-05 16:34:41 -0400

    Update overpass_graph.rb

--- lib/overpass_graph.rb
+++ lib/overpass_graph.rb
@@ -6,7 +6,7 @@ def overpass_graph(north, east, south, west, directed: true, filter_by_allowing:
     '''
     Primary function for the library. The user may specify whether to filter by allowing or not, in addition to  a few other options.
     If they choose to filter by allowing certain road types, the get roads function will be passed an empty array for disallowed values.
-    If they choose to filter by disallowing certain road types, the get roads function will be passed an empty array for the allowed values.
+    If they choose to filter by disallowing certain road types, the get roads function will be passed an empty array for allowed values.
     :return: adjacency map representation of a graph
     '''
     allowed_values = []
